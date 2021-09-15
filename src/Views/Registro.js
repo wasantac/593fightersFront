aff65530afe0569a75d525218c1e87ad16300d57
wasantac <34143200+wasantac@users.noreply.github.com>
2021-09-14 21:08:54 -0500

    registro

--- src/Views/Registro.js
+++ src/Views/Registro.js
@@ -11,12 +11,20 @@ const Registro = () => {
     const history = useHistory();
     let handleSubmit = (event) => {
         let username = event.target[0].value;
-        let password = event.target[1].value;
-        let validate = event.target[2].value;
+        let nick = event.target[1].value;
+        let nombre = event.target[2].value;
+        let correo = event.target[3].value;
+        let whats = event.target[4].value;
+        let password = event.target[5].value;
+        let validate = event.target[6].value;
         if(password ===validate){
             axios.post(`/login/registro`,{
                 user: username,
                 password,
+                nombre,
+                correo,
+                whats,
+                nick
             },).then(res => {
                 history.push(`/login`)
             })
@@ -31,11 +39,36 @@ const Registro = () => {
             <div className="card shadow">
             <img src={logo} alt="593Logo" className="imgtorneo"></img>
             <Form className="px-5 pt-5 pb-5 tarjeta" onSubmit={handleSubmit}>
+                <h1 className="text-center">Registro</h1>
                 <Row>
                 <Col md={12}>
                 <Form.Group className="mb-2" controlId="username">
                     <Form.Label>Usuario</Form.Label>
                     <Form.Control type="text" name="username" required></Form.Control>
+                </Form.Group> 
+                </Col>
+                <Col md={12}>
+                <Form.Group className="mb-2" controlId="nick">
+                    <Form.Label>Nick (PS4)</Form.Label>
+                    <Form.Control type="text" name="nick" required></Form.Control>
+                </Form.Group> 
+                </Col>
+                <Col md={12}>
+                <Form.Group className="mb-2" controlId="nombre">
+                    <Form.Label>Nombre</Form.Label>
+                    <Form.Control type="text" name="nombre" required></Form.Control>
+                </Form.Group> 
+                </Col>
+                <Col md={12}>
+                <Form.Group className="mb-2" controlId="correo">
+                    <Form.Label>Correo</Form.Label>
+                    <Form.Control type="email" name="correo" required></Form.Control>
+                </Form.Group> 
+                </Col>
+                <Col md={12}>
+                <Form.Group className="mb-2" controlId="whats">
+                    <Form.Label>Whatsapp</Form.Label>
+                    <Form.Control type="text" name="whats" required></Form.Control>
                 </Form.Group> 
                 </Col>
                 <Col md={6}>
